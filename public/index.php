c5aaabd98acf7441a2dfe9313795300a339aae93
Thiago Victor <thiago_vick@yahoo.com.br>
2015-04-27 20:20:25 -0300

    Corrigindo retorno Json

--- public/index.php
+++ public/index.php
@@ -2,23 +2,14 @@
 
 require_once __DIR__.'/../bootstrap.php';
  
-$app->get('/clientes', function (){
+$app->get('/clientes', function () use ($app){
     $clientes = [
         [nome=>"TSA Tecnologia",email=>"informatica@tsasistemas.com.br",doc=>"41.857.754/0001-68"],
         [nome=>"Anvisa Sistemas",email=>"informatica@anvisasistemas.com",doc=>"41.358.754/0001-00"],
         [nome=>"EPC Vistoria",email=>"contato@epcvistorias.com.br",doc=>"31.892.854/0001-18"],
         [nome=>"Marista Tecnologia",email=>"vendas@mtecnologia.com.br",doc=>"41.998.555/0001-10"]
     ];
-    
-    foreach ($clientes as $cliente) {
-        $retorno .= "{<br>";
-        foreach ($cliente as $key => $value) { 
-            $retorno .= '&nbsp;&nbsp;&nbsp;"'.$key.'":"'.$value.'",<br>';   
-        }
-        $retorno .= "}<br>";
-    }
-    return $retorno;
-    
+    return $app->json($clientes);    
 });
 $app->run();
 
